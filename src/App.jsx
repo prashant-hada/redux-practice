7e5066717acc3e269d5ead8493cef2e0d6ac274f
Prashant <hadaprashant2002@gmail.com>
2024-12-27 23:23:14 +0530

    created Todo List compent and other things

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import AddTodo from './components/AddTodo'
+import Todos from './components/Todos'
 
 function App() {
   return (
-    <div>App</div>
+  <div className='h-sceen w-full flex flex-col items-center justify-center p-6 mt-6'>
+    <h1 className='text-3xl'>Learning Redus Toolkit</h1>
+    <AddTodo />
+    <Todos />
+  </div>    
+
   )
 }
 
